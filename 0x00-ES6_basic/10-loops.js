c6c8fb8b5036003942321c98ee379a83ad608251
Victoriabunmi72 <bhummhie97@gmail.com>
2023-07-24 19:47:38 +0100

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,13 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // Loop through each element (value) of the array using the for...of loop
-  for (let value of array) {
-    // Append the appendString to each value
-    value = appendString + value;
+  const narray = [];
+  for (const item of array) {
+    narray.push((appendString + item));
   }
-
-  // The original array remains unchanged since 'value' is a copy of the array element
-  // The changes made to 'value' inside the loop do not affect the original array
-
-  // Return the modified array
-  return array;
+  return narray;
 }

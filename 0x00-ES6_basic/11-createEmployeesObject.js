4ded609d17d292b4d6cca9d60992ac3fb8ddfc7e
Victoriabunmi72 <bhummhie97@gmail.com>
2023-07-24 19:51:06 +0100

    11-createEmployeesObject.js

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,5 @@
+const department = 'HR';
+const employeesArray = ['John', 'Jane', 'Mike'];
+
+const employeesObject = createEmployeesObject(department, employeesArray);
+console.log(employeesObject);

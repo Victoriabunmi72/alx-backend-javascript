d7e3f32e373035a2059c4f035801adb83ec14f31
Victoriabunmi72 <bhummhie97@gmail.com>
2023-09-22 02:06:57 +0100

    2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,21 +1,18 @@
-/**
-* Tests for Calculation function
-*/
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
   describe('type == "SUM"', () => {
     it('equal positive numbers', () => {
-      expect(calculateNumber('SUM', 3.0, 2.0)).to.equal(5);
+      expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4);
     });
 
     it('equal positive numbers (alternate)', () => {
-      expect(calculateNumber('SUM', 3.3, 1.8)).to.equal(5);
+      expect(calculateNumber('SUM', 2.3, 1.8)).to.equal(4);
     });
 
     it('equal negative numbers', () => {
-      expect(calculateNumber('SUM', -3.0, -2.0)).to.equal(-5);
+      expect(calculateNumber('SUM', -2.0, -2.0)).to.equal(-4);
     });
 
     it('equal negative numbers (alternate)', () => {
